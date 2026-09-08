c8f5dffef03cb672ba3e50069ddfa7c3c5051da2
rajbhowmick27 <52496816+rajbhowmick27@users.noreply.github.com>
2026-09-08 10:00:45 +0530

    Add Medium Java Solution for 56. Merge Intervals from LeetCode

--- LeetCode/Patterns/Arrays/56. Merge Intervals/Solution.java
+++ LeetCode/Patterns/Arrays/56. Merge Intervals/Solution.java
@@ -5,17 +5,24 @@ class Solution {
         }
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
-        int index = 0;
+        List<int[]> res = new ArrayList<>();
+
+        int[] curInterval = intervals[0];
+        res.add(curInterval);
+
         for(int i=1;i<intervals.length;i++){
-            if(intervals[index][1] >= intervals[i][0]){
-                intervals[index][1] = Math.max(intervals[index][1],intervals[i][1]);
-            } else{
-                index++;
-                intervals[index] = intervals[i];
+            int currentEnd = curInterval[1];
+            int nextStart = intervals[i][0];
+            int nextEnd = intervals[i][1];
+            
+            if(currentEnd >= nextStart){
+                curInterval[1] = Math.max(currentEnd,nextEnd);
+            } else {
+                curInterval = intervals[i];
+                res.add(curInterval);
             }
         }
-
-        return Arrays.copyOf(intervals,index + 1);
         
+        return res.toArray(new int[res.size()][]);
     }
 }
